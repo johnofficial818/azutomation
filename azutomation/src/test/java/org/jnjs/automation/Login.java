fbe28bb427eb9e93662709912c1c45fdd91845e7
John <johnofficial818@gmail.com>
2022-12-16 01:18:58 +0530

    changed

--- azutomation/src/test/java/org/jnjs/automation/Login.java
+++ azutomation/src/test/java/org/jnjs/automation/Login.java
@@ -9,7 +9,7 @@ public class Login {
 	@Test
     public void evaluatesExpression() {
        
-       int sum = 8;
-       assertEquals(8, sum);
+       int sum = 9;
+       assertEquals(9, sum);
     }
 }
